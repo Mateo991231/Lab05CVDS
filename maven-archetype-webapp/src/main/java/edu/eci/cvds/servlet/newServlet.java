03ae4b938a681305adab234a68498e54b9fd8810
unknown <mateo.quintero@labinfo.is.escuelaing.edu.co>
2020-02-27 09:57:09 -0500

    avance lab

--- maven-archetype-webapp/src/main/java/edu/eci/cvds/servlet/newServlet.java
+++ maven-archetype-webapp/src/main/java/edu/eci/cvds/servlet/newServlet.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(
-        urlPatterns="/helloServlet"
+        urlPatterns="/servlet"
 )
 
 public class newServlet extends HttpServlet {
@@ -49,5 +49,40 @@ public class newServlet extends HttpServlet {
         }
         responseWriter.flush();
     }
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int status =HttpServletResponse.SC_OK;
+        Writer responseWriter=null;
+        int validId=-1;
+        try {
+            responseWriter = resp.getWriter();
+            Optional<String> optId = Optional.ofNullable(req.getParameter("id"));
+            String id = optId.isPresent() && !optId.get().isEmpty() ? optId.get() : "-1";
+            try {
+                validId=Integer.parseInt(id);
+            }catch(NumberFormatException e) {
+                status = HttpServletResponse.SC_BAD_REQUEST;
+            }
+            if (status==200) status = (validId < 1 || validId > 200) ? HttpServletResponse.SC_NOT_FOUND :HttpServletResponse.SC_OK;
+            status = (req.getParameter("id") == null)? HttpServletResponse.SC_BAD_REQUEST:status;
+            resp.setStatus(status);
+        }catch(MalformedURLException e) {
+            status=HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
+        }catch(Exception e) {
+            status=HttpServletResponse.SC_BAD_REQUEST;
+        }
+        if(status==200) {
+            ArrayList<Todo> todoList = new ArrayList();
+            todoList.add(Service.getTodo(validId));
+            String htmlTable=Service.todosToHTMLTable(todoList);
+            responseWriter.write(htmlTable);
+        }else {
+            responseWriter.write("Error "+status);
+        }
+        responseWriter.flush();
+    }
+
+
+
 
 }
